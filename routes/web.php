a7850a2a3205c97306e4837ce58554b37deae715
Zuhriyahning <zuhriyaning5@gmail.com>
2023-06-07 15:07:11 +0700

    autentikasi

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +27,15 @@ Route::post('/mahasiswa/store',[MahasiswaController::class,'store']);
 Route::get('/mahasiswa/{id}/edit',[MahasiswaController::class,'edit']);
 Route::put('/mahasiswa/{id}',[MahasiswaController::class,'update']);
 Route::delete('/mahasiswa/{id}',[MahasiswaController::class,'destroy']);
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
